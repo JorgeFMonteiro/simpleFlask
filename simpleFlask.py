f61a9df3c4ef190b1caeb6d2380bd5f8242f8ff0
Jorge Monteiro <jorge.m@spotqa.com>
2022-06-28 17:23:21 +0100

    Initial commit

---
+++ simpleFlask.py
@@ -0,0 +1,18 @@
+from time import sleep
+from flask import Flask
+
+app = Flask(__name__)
+
+RESPONSE_DELAY=1
+
+
+@app.route("/")
+def hello_world():
+    sleep(RESPONSE_DELAY)
+    return "Returning with delay"
+
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
+
